058cbb7992c1b965d1611b30385cc90642d7c565
zurotrust <flyer-fritter-icky@duck.com>
2025-02-14 00:36:45 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,8 +3,8 @@
 # shellcheck disable=SC2086
 
 set -eE
-confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-confhome_cn=https://raw.githubusercontent.com/bin456789/reinstall/main
+confhome=https://raw.githubusercontent.com/zurotrust/reinstall/main
+confhome_cn=https://raw.githubusercontent.com/zurotrust/reinstall/main
 # confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
 # 默认密码
